29fda0e3c19329902be713fdbebe2f3fc8df5826
Superdranzer <47878236+Superdranzer@users.noreply.github.com>
2021-12-30 14:28:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import App from './App';
+import ReactDom from 'react-dom';
+import 'antd/dist/antd.css';
+import store from './App/store';
+ReactDom.render(
+    <Router>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </Router>,
+    document.getElementById('root')
+);
